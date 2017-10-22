5bde30287ceddf2b362235ab02b20d5c8b5e2eaf
katalyzator <web.coder96@gmail.com>
2017-10-22 15:27:44 +0600

    added new logics

--- maub/urls.py
+++ maub/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from main.views import IndexView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^$', IndexView.as_view(), name='index'),
 ]
